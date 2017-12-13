002799e38235bde62cc4669e624a9150353c3af7
Oláh Tamás <tolah94@gmail.com>
2017-12-13 11:23:36 +0100

    Fixed csv spaces and modified data_manager so it can find the file now

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,8 @@
 import connection
 import os
 
-ANSWER_DATA_FILE_PATH = os.getenv('ANSWER_DATA_FILE_PATH') if 'ANSWER_DATA_FILE_PATH' in os.environ else 'answer.csv'
-QUESTION_DATA_FILE_PATH = os.getenv('QUESTION_DATA_FILE_PATH') if 'QUESTION_DATA_FILE_PATH' in os.environ else 'question.csv'
+ANSWER_DATA_FILE_PATH = os.getenv('ANSWER_DATA_FILE_PATH') if 'ANSWER_DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
+QUESTION_DATA_FILE_PATH = os.getenv('QUESTION_DATA_FILE_PATH') if 'QUESTION_DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
 QUESTION_DATA_HEADER = ['id', 'submisson_time', 'view_number', 'title', 'message']
 
 
