121d7dd86f19eb89dfa1aac944d5153dea249e10
Peter Kondacs <kondacspeter@gmail.com>
2017-12-14 12:02:01 +0100

    Created a better design for the question details functionality.

--- data_manager.py
+++ data_manager.py
@@ -1,13 +1,14 @@
 import connection
 import os
+import util
 
 ANSWER_DATA_FILE_PATH = os.getenv('ANSWER_DATA_FILE_PATH') if 'ANSWER_DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
 ANSWER_DATA_HEADER = ['id', 'submisson_time', 'question_id', 'message']
-ANSWER_DATA_HEADER_QUESTIONS_PAGE = ['id', 'submisson_time', 'message']
+ANSWER_DATA_HEADER_QUESTIONS_PAGE = ['submisson_time', 'message']
 QUESTION_DATA_FILE_PATH = os.getenv('QUESTION_DATA_FILE_PATH') if 'QUESTION_DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
 QUESTION_DATA_HEADER = ['id', 'submisson_time', 'view_number', 'title', 'message']
 FANCY_QUESTION_DATA_HEADER = ['ID', 'Submission time', 'View number', 'Title', 'Message']
-QUESTION_DATA_HEADER_QUESTIONS_PAGE = ['submisson_time', 'view_number', 'title', 'message']
+QUESTION_DATA_HEADER_QUESTIONS_PAGE = ['submisson_time', 'view_number', 'message']
 
 
 def reverse_data(filename):
@@ -17,13 +18,14 @@ def reverse_data(filename):
 
 
 def get_question_data(question_id):
-    data = connection.csv_reader(QUESTION_DATA_FILE_PATH)[int(question_id)]
-    return data
+    data_table = util.convert_timestamp_to_date(connection.csv_reader(QUESTION_DATA_FILE_PATH))
+    question_data = data_table[int(question_id) - 1]
+    return question_data
 
 
 def get_answers_for_question(question_id):
-    answers = list(connection.csv_reader(ANSWER_DATA_FILE_PATH))
-    answers_for_question_id = [row for row in answers if row['question_id'] == question_id]
+    data_table = util.convert_timestamp_to_date(connection.csv_reader(ANSWER_DATA_FILE_PATH))
+    answers_for_question_id = [row for row in data_table if row['question_id'] == question_id]
     return answers_for_question_id
 
 
@@ -34,3 +36,15 @@ def add_new_answer_to_csv(new_answer):
 def get_new_answer_id():
     last_answer_id = connection.csv_reader(ANSWER_DATA_FILE_PATH)[-1]['id']
     return last_answer_id
+
+
+def make_display_header(header_list):
+    return [header.capitalize().replace('_',' ') for header in header_list]
+
+
+def increment_view_number(question_id):
+    table = connection.csv_reader(QUESTION_DATA_FILE_PATH)
+    view_number_integer = int(table[int(question_id)-1]['view_number'])
+    view_number_integer += 1
+    table[int(question_id) - 1]['view_number'] = view_number_integer
+    connection.write_to_file(QUESTION_DATA_FILE_PATH, table)
--- server.py
+++ server.py
@@ -30,18 +30,23 @@ def route_add_question():
 @app.route('/question/<question_id>')
 def route_question_detail(question_id):
     question_header = data_manager.QUESTION_DATA_HEADER_QUESTIONS_PAGE
+    question_header_display = data_manager.make_display_header(question_header)
+    data_manager.increment_view_number(question_id)
     question_data = data_manager.get_question_data(question_id)
     answer_header = data_manager.ANSWER_DATA_HEADER_QUESTIONS_PAGE
+    answer_header_display = data_manager.make_display_header(answer_header)
     answers = data_manager.get_answers_for_question(question_id)
     add_answer_url = url_for('route_add_answer', question_id=question_id)
-    return render_template('question_detail.html', question_id=question_id, question_header=question_header, add_answer_url=add_answer_url, question_data=question_data, answer_header=answer_header, answers=answers)
+    return render_template('question_detail.html', question_id=question_id, question_header=question_header, add_answer_url=add_answer_url, question_data=question_data, answer_header=answer_header, answers=answers, question_header_display=question_header_display, answer_header_display=answer_header_display)
 
 
 @app.route('/question/<question_id>/new-answer', methods=['POST', 'GET'])
 def route_add_answer(question_id):
     question_header = data_manager.QUESTION_DATA_HEADER_QUESTIONS_PAGE
+    question_header_display = data_manager.make_display_header(question_header)
     question_data = data_manager.get_question_data(question_id)
     answer_header = data_manager.ANSWER_DATA_HEADER_QUESTIONS_PAGE
+    answer_header_display = data_manager.make_display_header(answer_header)
     answers = data_manager.get_answers_for_question(question_id)
     add_answer_url = url_for('route_add_answer', question_id=question_id)
     question_detail_url = url_for('route_question_detail', question_id=question_id)
@@ -49,7 +54,7 @@ def route_add_answer(question_id):
         new_answer = request.form.to_dict()
         data_manager.add_new_answer_to_csv(new_answer)
         return redirect(question_detail_url)
-    return render_template('add_answer.html', question_id=question_id, question_header=question_header, add_answer_url=add_answer_url, question_data=question_data, answer_header=answer_header, answers=answers)
+    return render_template('add_answer.html', question_id=question_id, question_header=question_header, add_answer_url=add_answer_url, question_data=question_data, answer_header=answer_header, answers=answers, question_header_display=question_header_display, answer_header_display=answer_header_display)
 
 
 if __name__ == '__main__':
